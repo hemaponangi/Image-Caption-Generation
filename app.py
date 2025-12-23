cea1d42280a89a2ef5354c6fe21d35678c2f8704
hemaponangi <hemaponangi@gmail.com>
2025-12-23 18:18:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+import torch
+
+app = Flask(__name__)
+
+# Load pre-trained model
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-base"
+)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    caption = ""
+    if request.method == "POST":
+        image_file = request.files["image"]
+
+        if image_file:
+            image = Image.open(image_file).convert("RGB")
+
+            inputs = processor(image, return_tensors="pt")
+            output = model.generate(**inputs)
+
+            caption = processor.decode(output[0], skip_special_tokens=True)
+
+    return render_template("index.html", caption=caption)
+
+if __name__ == "__main__":
+    app.run(debug=True)
